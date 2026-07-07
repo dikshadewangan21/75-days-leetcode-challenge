d324003a72ffe43689ed6ebbd501b0051b078d6f
Diksha Dewangan <dewangandiksha386@gmail.com>
2026-07-07 23:46:01 +0530

    Time: 0 ms (100%), Space: 12 MB (65.19%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        
+        for (int x : arr)
+            freq[x]++;
+        
+        unordered_set<int> st;
+        
+        for (auto &it : freq) {
+            if (st.count(it.second))
+                return false;
+            st.insert(it.second);
+        }
+        
+        return true;
+    }
+};
